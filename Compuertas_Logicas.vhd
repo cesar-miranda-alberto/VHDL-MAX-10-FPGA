3111820bc0a11e956d80fc3b9aadda5a30334f5f
cesar-miranda-alberto <142947777+cesar-miranda-alberto@users.noreply.github.com>
2024-02-08 14:17:15 -0600

    Add files via upload

---
+++ Compuertas_Logicas.vhd
@@ -0,0 +1,38 @@
+---------------------------------------------------------------------------------------------
+-- 					***** COMPUERTAS LÓGICAS *****														  -
+--																														  -
+--		El presente código desarrollado en vhdl tiene como finalidad la implementación		  -
+--  	de las compuertas lógicas para posteriormete desarrollar el testbench 					  -
+--		correspondiente.																			              -
+--    Fecha: 07-Enero-2023																						  -
+-- 	Versión: 1.0																								  -
+---------------------------------------------------------------------------------------------
+
+library IEEE;																										-- Incluye librerias correspondientes del IEEE
+use IEEE.std_logic_1164.all;																					-- Agrega la libreria del IEEE a utilizar
+
+entity Compuertas_Logicas is																					-- Creación de la entidad y asignación de nombre
+		port(																											-- Inicio de declaración de puertos de entrada y salida
+				A : in STD_LOGIC;																					-- Entrada de un bit
+				B : in STD_LOGIC;
+				RNOT : out STD_LOGIC;																			-- Salida de un bit
+				RAND : out STD_LOGIC;
+				RNAND : out STD_LOGIC;
+				REOR  : out STD_LOGIC;
+				RNOR  : out STD_LOGIC;
+				RXOR  : out STD_LOGIC;
+				RXNOR : out STD_LOGIC 
+				);
+end Compuertas_Logicas;																							-- Fin de la entidad 
+
+architecture simple of Compuertas_Logicas is																-- Declaración "SIMPLE" de la arquitectura 
+begin																													-- Inicio de la arquitectura
+		RNOT <= not (A);																							-- Ejecuta las compuertas lógicas dentro de la arquitectura
+		RAND <= A and B;
+		RNAND <= A nand B;
+		REOR <= A or B;
+		RNOR <= A nor B;
+		RXOR <= A xor B;
+		RXNOR <= A xnor B;	 																					
+		
+end simple;																											-- Fin de la arquitectura
---
+++ Compuertas_Logicas_Testbench.vhd
@@ -0,0 +1,74 @@
+---------------------------------------------------------------------------------------------
+-- 					***** COMPUERTAS LÓGICAS TESTBENCH *****											  -
+--																														  -
+--		El presente código desarrollado en vhdl tiene como finalidad la implementación		  -
+--  	del testbech correspondiente a las compuertas lógicas en vhdl desarrollado				  -
+--		anteriormente, con el fin de analizar el funcionamiento .									  -
+--    Fecha: 07-Enero-2023																						  -	
+-- 	Versión: 1.0																								  -
+---------------------------------------------------------------------------------------------
+
+library IEEE;																										  -- Incluye librerias correspondientes del IEEE
+use IEEE.STD_LOGIC_1164.ALL;																					  -- Agrega la libreria del IEEE a utilizar
+
+entity Compuertas_Logicas_Testbench is																		  -- Creación de la entidad correspondiente al Testbench
+
+end Compuertas_Logicas_Testbench;																			  -- Fin de la entidad del testbench
+
+architecture simple of Compuertas_Logicas_Testbench is												  -- Inicio de la arquitectura simple correspondiente a la entidad
+
+ component Compuertas_Logicas																					  -- Lista de componentes
+		port( 																										  -- Inicio de los puertos
+				A : in STD_LOGIC;																					  -- Puertos de entrada de un solo bit
+				B : in STD_LOGIC;
+				RNOT : out STD_LOGIC;																			  -- Puertos de salida de un solo bit
+				RAND : out STD_LOGIC;
+				RNAND : out STD_LOGIC;
+				REOR  : out STD_LOGIC;
+				RNOR  : out STD_LOGIC;
+				RXOR  : out STD_LOGIC;
+				RXNOR : out STD_LOGIC 
+			);
+end component;																										  -- Fin de la lista de componentes 
+
+signal A : STD_LOGIC:= '0';																					  -- Creación de las señales de entrada tipo signal con valor inicial
+signal B : STD_LOGIC:= '0';
+	
+signal RNOT  : STD_LOGIC;																						  -- Creación de las señales de entrada tipo signal
+signal RAND  : STD_LOGIC;
+signal RNAND : STD_LOGIC;
+signal REOR  : STD_LOGIC;
+signal RNOR  : STD_LOGIC;
+signal RXOR  : STD_LOGIC;
+signal RXNOR : STD_LOGIC;
+
+begin 																												   -- Inicio de la arquitectura
+U0: Compuertas_Logicas Port map (																				-- Instanciación de la entidad Compuertas_Logicas
+	A => A,																												-- Conexión de los puertos de la entidad Compuertas_Logicas_Testbench con la entidad Compuertas_Logicas
+	B => B,
+	RNOT  => RNOT,
+	RAND  => RAND,
+	RNAND => RNAND,
+	REOR  => REOR,
+	RNOR  => RNOR,
+	RXOR  => RXOR,
+	RXNOR => RXNOR	
+);
+
+process begin																											-- Inicio del process
+
+	A <= '0';																											-- Valores asignados a los puertos de entrada
+	B <= '0';
+	wait for 100 ns;																									-- Retardo para generar cambios en valores de las señales
+	A <= '1';
+	B <= '0';
+	wait for 100 ns;
+	A <= '0';
+	B <= '1';
+	Wait for 100 ns;
+	A <= '1';
+	B <= '1';
+	wait;
+	
+	end process;																										-- Fin del process
+end simple; 																											-- Fin de la arquitectura simple
