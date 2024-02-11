73beaeb869d2a05c3145b9db9febf7495803ffef
cesar-miranda-alberto <142947777+cesar-miranda-alberto@users.noreply.github.com>
2024-02-11 16:29:44 -0600

    Multiplexor4_a_1_Testbench.vhd

--- Multiplexor4_a_1_Testbench.vhd
+++ Multiplexor4_a_1_Testbench.vhd
@@ -1,55 +1,66 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+---------------------------------------------------------------------------------------------
+-- 				***** MULTIPLEXOR 4 A 1 TESTBENCH *****			    -  					
+--											    -																		  -
+--	El presente código desarrollado en vhdl tiene como finalidad la implementación      -
+-- 	de un Testbench para analizar el funcionamiento de un multiplexor 4 a 1 de una      -
+--	longitud de un solo bit. Partiendo de un código previamente desarrollado.	    -
+-- 			 								    -
+-- 	Fecha: 09-Enero-2023								    -													  -
+-- 	Versión: 1.0									    -
+---------------------------------------------------------------------------------------------
 
-entity Multiplexor4_a_1_Testbench is
+library IEEE;															-- Incluye librerias relacionadas al IEEE
+use IEEE.STD_LOGIC_1164.ALL;													-- Incluye libreria del IEEE
 
-end Multiplexor4_a_1_Testbench;
+entity Multiplexor4_a_1_Testbench is												-- Creación de la entidad del testbench
 
-architecture simple of Multiplexor4_a_1_Testbench is
+end Multiplexor4_a_1_Testbench;													-- Fin del la entidad
 
-	component Multiplexor4_a_1
+architecture simple of Multiplexor4_a_1_Testbench is										-- Creación de la arquitectura 
+
+	component Multiplexor4_a_1												-- Lista de componentes
 					port(
-							A : in STD_LOGIC;
+							A : in STD_LOGIC;							-- Creación de puertos de entrada con longitud de un bit 
 							B : in STD_LOGIC;
 							C : in STD_LOGIC;
 							D : in STD_LOGIC;
-							Sel : in STD_LOGIC_VECTOR(1 downto 0);
-							Salida : out STD_LOGIC
+							Sel : in STD_LOGIC_VECTOR(1 downto 0);					-- Creación de puerto de entrada con longitud de dos bits
+							Salida : out STD_LOGIC							-- Creación de puerto de salida
 						);
 					
-	end component;
+	end component;														-- Fin de la lista de componentes
 	
-	signal A : STD_LOGIc:= '1';
+	signal A : STD_LOGIc:= '1';												-- Creación de signals correspondientes a la lista de componentes con estado lógico 
 	signal B : STD_LOGIC:= '0';
 	signal C : STD_LOGIC:= '1';
 	signal D : STD_LOGIC:= '0';
 	signal Sel : STD_LOGIC_VECTOR(1 downto 0);
 	signal Salida : STD_LOGIC;
 	
-	begin
+	begin															-- Inicio de la arquitectura
 	
-		U0: Multiplexor4_a_1 
+		U0: Multiplexor4_a_1 												-- Instanciación del Multiplexor
 		port map(
-					A => A,
+					A => A,											-- Conexión de componentes del testbench con el multiplexor
 					B => B,
 					C => C,
 					D => D,
 					Sel => Sel,
 					Salida => Salida
 				);
-		process
-		 begin
+		process											
+		 begin														-- Inicio del proceso
 		 
-			Sel <="00";
-			wait for 100 ns;
+			Sel <="00";												-- Asigna valor al selector
+			wait for 100 ns;											-- Establece retardo entre los cambios
 			Sel <="01";
 			wait for 100 ns;
 			Sel <="10";
 			wait for 100 ns;
 			Sel <= "11";
 			wait for 100 ns;
-			wait;		
-		end process;
+			wait;													-- Fin de los cambios
+		end process;													-- Fin del proceso
 		
-end simple;
+end simple;															-- Fin de la arquitectura
 					
