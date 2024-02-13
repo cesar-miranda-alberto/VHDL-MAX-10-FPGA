d54842d5b72a2677092027ee21f6e8936984967b
cesar-miranda-alberto <142947777+cesar-miranda-alberto@users.noreply.github.com>
2024-02-13 14:49:43 -0600

    Nombre_Compuertas_Logicas.vhd

--- Nombre_Compuertas_Logicas.vhd
+++ Nombre_Compuertas_Logicas.vhd
@@ -1,7 +1,19 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+---------------------------------------------------------------------------------------------
+-- 				***** NOMBRE COMPUERTAS LÓGICAS *****			    -
+--											    -
+--	El presente código desarrollado en vhdl tiene como finalidad la implementación      -
+--  	de mapas de karnaugh a través los displays siete segmentos exhibiendo el nombre	    -
+--	CESAR partiendo de las ecuaciones boleanas. El proyecto esta desarrollado e 	    -	
+--	implementado para un FPGA MAX 10.						    -
+--											    -
+--      Fecha: 09-Enero-2023								    -
+-- 	Versión: 1.0									    -
+---------------------------------------------------------------------------------------------
 
-entity Nombre_Compuertas_Logicas is
+library IEEE;													-- Incluye librerias relacionadas al IEEE
+use IEEE.STD_LOGIC_1164.ALL;											-- Agrega biblioteca del IEEE
+
+entity Nombre_Compuertas_Logicas is										-- Creación de lista de puertos de entrada y salida
 			port(
 					x : in STD_LOGIC;
 					y : in STD_LOGIC;
@@ -14,11 +26,11 @@ entity Nombre_Compuertas_Logicas is
 					F : out STD_LOGIC;
 					G : out STD_LOGIC
 			);
-end Nombre_Compuertas_Logicas;
+end Nombre_Compuertas_Logicas;											-- Fin de la entidad
 
-architecture simple of Nombre_Compuertas_Logicas is
+architecture simple of Nombre_Compuertas_Logicas is								-- Creación de la arquitectura
 begin
-
+														-- Ecuaciones boleanas
 			A <= not ((not x) or (not y and (not z)));
 			B <= not ((not x) and y and z);
 			C <= not ((not x) and y);
@@ -27,4 +39,4 @@ begin
 			F <= not ((not x) or (not y and not z));
 			G <= not ((not x and z) or (not x and y));
 
-end simple;
+end simple;													-- Fin de la arquitectura
